80bc08f48490f6703b09e018299bc51b99ef3018
harryhuayuanchong <harryhuayuanchong@gmail.com>
2023-11-22 11:44:24 +0800

    file: hardhat.config.js modification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,15 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// Add optimizer & evmVersion here
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      }
+    }
+  },
 };
